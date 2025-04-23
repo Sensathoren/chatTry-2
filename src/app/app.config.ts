9d0111c20a4b85845b9b5fca0215962d33de4cd6
Sensathoren <koskidennis@outlook.com>
2025-04-23 16:54:40 -0700

    fixed broken file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,14 +11,14 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
+  
       provideFirebaseApp(() => initializeApp(environment.firebase)),
       provideFirestore(() => getFirestore()),
       provideAuth(() => getAuth()),
       provideFunctions(() => getFunctions()),
       provideStorage(() => getStorage()),
       provideMessaging(() => getMessaging())
-    ),
+    ,
     provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-7490e","appId":"1:242769824570:web:22d8e78ed1b2d5b9f3de03","storageBucket":"friendlychat-7490e.firebasestorage.app","apiKey":"AIzaSyBOWgY_nceuI5YGH_81myjF2Cb47b0tk20","authDomain":"friendlychat-7490e.firebaseapp.com","messagingSenderId":"242769824570"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
