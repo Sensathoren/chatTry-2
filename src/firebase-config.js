c5219eaa9ea31b862b70c83c3146a9052d789e03
Sensathoren <koskidennis@outlook.com>
2025-04-23 17:32:54 -0700

    shot in dark

---
+++ src/firebase-config.js
@@ -0,0 +1,16 @@
+// export const environment = {
+//     firebase: {
+//         apiKey: "AIzaSyBOWgY_nceuI5YGH_81myjF2Cb47b0tk20",
+
+//         authDomain: "friendlychat-7490e.firebaseapp.com",
+      
+//         projectId: "friendlychat-7490e",
+      
+//         storageBucket: "friendlychat-7490e.firebasestorage.app",
+      
+//         messagingSenderId: "242769824570",
+      
+//         appId: "1:242769824570:web:22d8e78ed1b2d5b9f3de03",
+//     },
+//   };
+  
