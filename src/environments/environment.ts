b50b4f051d97af1eff5d991764e88ff6282fde60
Sensathoren <koskidennis@outlook.com>
2025-04-23 18:36:48 -0700

    maybe this time

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
+    apiKey: "AIzaSyBOWgY_nceuI5YGH_81myjF2Cb47b0tk20",
+    authDomain: "friendlychat-7490e.firebaseapp.com",
+    databaseURL: 'https://friendlychat-7490e.firebaseio.com',
+    projectId: "friendlychat-7490e",
+    storageBucket: "friendlychat-7490e.firebasestorage.app",
+    messagingSenderId: "242769824570",
+    appId: "1:242769824570:web:22d8e78ed1b2d5b9f3de03",
     measurementId: 'G-MEASUREMENT_ID',
   },
 };
